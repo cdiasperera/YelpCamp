be4007bf6311b3f6a64da99ecc7a129cbba2826b
Channa Dias Perera <cdiasperera@gmail.com>
2019-10-07 17:46:22 +0530

    Added list of campgrounds and images

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static('public'));
+app.use(express.static(__dirname + "/public"));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
