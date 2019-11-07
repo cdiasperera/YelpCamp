a10e71f088944a78ecf5155a1d060629e926abc5
Channa <cdiasperera@gmail.com>
2019-11-07 12:32:59 +0530

    Added password requirements for user to see

--- public/scripts/passwordValidator.js
+++ public/scripts/passwordValidator.js
@@ -1,1 +1,22 @@
-console.log("Hi");
+;(function() {
+  "use strict";
+    var errorMessage = "Sorry!";
+
+  $(disableValidationPopup);
+  $(clearFormOnInvalid);
+  
+  function disableValidationPopup() {
+    document.addEventListener('invalid', (function () {
+      return function (e) {
+        $("input[name=password]").focus();
+        e.preventDefault();
+      };
+    })(), true);
+  }
+
+  function clearFormOnInvalid() {
+    $("input[name=password").on("invalid", function() {
+      this.value = "";
+    });
+  }
+})();
