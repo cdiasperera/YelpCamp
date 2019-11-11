ab54ed1055729b06ff32462d40c66e54d3dc67be
Channa <cdiasperera@gmail.com>
2019-11-11 07:01:38 +0530

    Refactored disablepopups code into its own file

---
+++ public/scripts/disablePopups.js
@@ -0,0 +1,18 @@
+// REMOVE POPUP FOR FORMS
+$(disableValidationPopup);
+$(clearFormOnInvalid);
+
+function disableValidationPopup() {
+  document.addEventListener("invalid", (function () {
+    return function (e) {
+      $("input").focus();
+      e.preventDefault();
+    };
+  })(), true);
+}
+
+function clearFormOnInvalid() {
+  $("input").on("invalid", function() {
+    this.value = "";
+  });
+}
