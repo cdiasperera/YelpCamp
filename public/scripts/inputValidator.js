fc5d91ae9fba4b17045d368c76423e61b0d16c1b
Channa <cdiasperera@gmail.com>
2019-11-14 12:40:36 +0530

    Finished Real Time Validation for Usernames

--- public/scripts/inputValidator.js
+++ public/scripts/inputValidator.js
@@ -1,8 +1,9 @@
 "use strict";
 // CHANGE REQUIREMENT CSS BASED ON INPUT
-function InputValidator(input, requirementsHTML, requirements) {
+function InputValidator(input, requirementsHTML, rules) {
   this.input        = input;
   this.requirementsHTML = requirementsHTML;
+  this.rules = rules;
   this.prevValue    = "";
 
   this.rulesPassed = [];
@@ -13,58 +14,90 @@ function InputValidator(input, requirementsHTML, requirements) {
 
   this.checkValidation = (event) => {
     var currentInput = event.target.value;
-    var change = this.determineChange(currentInput);
-    this.checkRules(change);
-  }
 
-  this.determineChange = (currentInput) => {
-    var changedChar, changedType;
-    if (currentInput.length > this.prevValue.length) {
-      // character was added
-      changedChar = currentInput[currentInput.length - 1];
-      changedType = InputValidator.prototype.CHANGETYPES.ADD;
-    } else if (currentInput.length === this.prevValue.length) {
-      // character was changed
-      changedChar = currentInput[currentInput.length - 1];
-      changedType = InputValidator.prototype.CHANGETYPES.CHANGE;
-    } else {
-      // character was deleted
-      changedChar = this.prevValue[this.prevValue.length - 1]
-      changedType = InputValidator.prototype.CHANGETYPES.DELETE;
+    if (currentInput === "") {
+      this.prevValue = currentInput;
+      return requirementsHTML.forEach((requirement) => {
+        requirement.classList.remove("valid");
+        requirement.classList.remove("invalid");
+      })
     }
+    // If input is non-empty, check the rules
+    this.checkRules(currentInput);
 
     // Update previous value and return changed output
     this.prevValue = currentInput;
-    return {char: changedChar, type: changedType};
   }
 
-  this.checkRules = (change) => {
-    if (change.type === InputValidator.prototype.CHANGETYPES.ADD) {
-      
-    } else if (change.type === InputValidator.prototype.CHANGETYPES.CHANGE) {
+  this.checkRules = (currentInput) => {
+    this.rules.forEach((rule, index) => {
+      var targetClassList = requirementsHTML[index].classList;
+      var toggleAction = rule(currentInput);
+      toggleClasses(targetClassList, toggleAction, "valid", "invalid");
+    }) 
+  }
+}
 
+// An dictionary that contains functions to validate a rule. The
+// definition of the rule is implicit in the function.
+InputValidator.prototype.rules = {
+ // Length Rule
+  length: (currentValue, minLength, maxLength) => { 
+    if (minLength === undefined) {
+      minLength = 0;
+    } else if (maxLength === undefined) {
+      maxLength = Infinity;
+    }
+    if (currentValue.length > minLength && currentValue.length < maxLength) {
+      return true;
     } else {
-      // Deleted Key
+      return false;
+    }
+  },
+  // Special Characters rule
+  specialChar: (currentValue) => {
+    if (/^[A-Za-z0-9_]{1,15}$/.test(currentValue)) {
+      return true;
+    } else {
+      return false;
     }
   }
 }
 
-InputValidator.prototype = {
-  CHANGETYPES: {
-    ADD: "add",
-    CHANGE: "change",
-    DELETE: "delete"
+InputValidator.prototype.usernameRules = [
+  bindEndArgs(InputValidator.prototype.rules.length, 1,Infinity),
+  InputValidator.prototype.rules.specialChar
+];
+// HELPER FUNCTION
+// A function to toggle class states, depending on toggleAction.
+// If toggleAction is true, turn onClass on. Else turn offClass off.
+function toggleClasses(element, toggleAction, onClass, offClass) {
+  if(toggleAction) {
+    element.add(onClass);
+    element.remove(offClass);
+  } else {
+    element.add(offClass);
+    element.remove(onClass);
+  }
+}
+
+// Function to partially implement functions to be referenced later
+function bindEndArgs(fn, ...boundArgs) {
+  return (...args) => {
+    return fn(...args, ...boundArgs);
   }
 }
 
 var usernameValidator = new InputValidator(
   document.querySelector("input[name=username]"),
-  document.querySelector("input[name=password] + .input-requirements")  
+  document.querySelectorAll("input[name=username] + .input-requirements li"),
+  InputValidator.prototype.usernameRules  
 );
 
 var passwordValidator = new InputValidator(
   document.querySelector("input[name=password]"),
-  document.querySelector("input[name=password] + .input-requirements")
+  document.querySelectorAll("input[name=password] + .input-requirements li"),
+  InputValidator.prototype.passwordRules
 );
 
 usernameValidator.start();
