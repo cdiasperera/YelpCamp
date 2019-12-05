a823fbfc405e9186966490c1f46eb69aa4ff552a
Channa <cdiasperera@gmail.com>
2019-12-05 16:55:36 +0530

    Now if a camp's cost is 0/night, it shows 'Free' on the camp's page instead

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 app.use(flash())
 
-app.use(favicon(path.join(__dirname,'public','imgs','favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'imgs', 'favicon.ico')))
 
 // DB CONFIG
 const mongoURI = helper.makeMongoURI()
