79571b3d7ecf039a960f6b625d86d327146b7781
Channa <cdiasperera@gmail.com>
2019-12-10 19:06:49 +0530

    Turned on produc seeding

--- helper/index.js
+++ helper/index.js
@@ -65,7 +65,7 @@ helper.makeMongoURI = () => {
 helper.mostRecentUpdate = moment('20191211', 'YYYYMMDD')
 
 // Booleans that tells if we are seeding the database or not
-helper.producSeeding = false
+helper.producSeeding = true
 helper.devSeeding = false
 
 module.exports = helper
